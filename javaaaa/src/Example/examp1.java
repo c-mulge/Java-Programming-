100fa3ec19b450fa04e2c09d27c3dfa3fb0ae87b
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-12-09 21:22:49 +0530

    Delete javaaaa directory

--- javaaaa/src/Example/examp1.java
+++
@@ -1,9 +0,0 @@
-package Example;
-
-import Swing.MyFrame;
-
-public class examp1 {
-    public static void main(String[] args) {
-        new MyFrame();
-    }
-}
--- javaaaa/src/Swing/ex1.java
+++
@@ -1,7 +0,0 @@
-package Swing;
-
-public class ex1 {
-    public static void main(String[] args) {
-        new MyFrame();
-    }
-}
