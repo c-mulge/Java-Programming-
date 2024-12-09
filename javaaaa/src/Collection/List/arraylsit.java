b99cdd8679a39ef0ecf3dee34f0adc26635a4696
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-12-09 17:55:51 +0000

    updated

--- javaaaa/src/Collection/List/arraylsit.java
+++ javaaaa/src/Collection/List/arraylsit.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 
 public class arraylsit {
     public static void main(String[] args) {
-//        ArrayList<String> s=new ArrayList<>();  //arraylist
-//        s.add("Siddhesh");
-//        s.add("Rohan");
-//        s.add("Devu");
-//        System.out.println(s);
+    //    ArrayList<String> s=new ArrayList<>();  //arraylist
+    //    s.add("Siddhesh");
+    //    s.add("Rohan");
+    //    s.add("Devu");
+    //    System.out.println(s);
+    ArrayList<Integer> l = new ArrayList();
+    l.add(1);
+    l.add(2);
+    l.add(3);
+    System.out.println(l);
+    
     }
 }
